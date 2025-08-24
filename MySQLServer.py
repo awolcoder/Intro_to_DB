333bf5c95d3d6bfdee8207720b8717e4ad0b3c41
awolcoder <abdulbasitawil42@gmail.com>
2025-08-24 15:07:02 +0300

    first db project

--- MySQLServer.py
+++ MySQLServer.py
@@ -9,17 +9,19 @@ def create_database():
             user='',
             password='' 
         )
+
         if connection.is_connected():
             cursor = connection.cursor()
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
-        if connection.is_connected():
+        if 'cursor' in locals():
             cursor.close()
+        if 'connection' in locals() and connection.is_connected():
             connection.close()
 
 if __name__ == "__main__":
